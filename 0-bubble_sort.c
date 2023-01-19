5cc4ce8eb4aca7436d8d1c3dbacacb21288b58f7
John Diginee <Johndbizz@gmail.com>
2023-01-19 05:53:44 +0100

    Added source code files for 0-bubble_sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * swap_ints - Swap 2 integers in an array.
+ * @a: first integer to swap.
+ * @b: econd integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int temporary;
+
+	temporary = *a;
+	*a = *b;
+	*b = temporary;
+}
+
+/**
+ * bubble_sort - Function that sorts an array of ints.
+ * @array: array of integers to sort.
+ * @size: size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+    length = size;
+	bool bubbly = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (i = 0; i < length - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		length--;
+	}
+}
