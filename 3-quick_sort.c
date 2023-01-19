dd59a2eda0ddb58802101c7426dc446ee696f574
John Diginee <Johndbizz@gmail.com>
2023-01-19 21:56:25 +0100

    Added source code files for 3-quick_sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,76 @@
+#include "sort.h"
+
+/**
+ * swap - Swap two integers in an array
+ * @array: the integer array to sort
+ * @size: the size of the array
+ * @a: The address of first value
+ * @b: The address of second value
+ *
+ * Return: void
+ */
+void swap(int *array, size_t size, int *a, int *b)
+{
+	if (*a != *b)
+	{
+		*a = *a + *b;
+		*b = *a - *b;
+		*a = *a - *b;
+		print_array((const int *)array, size);
+	}
+}
+
+/**
+ * lomuto_partition - partitions the array
+ * @array: the integer array to sort
+ * @size: the size of the array
+ * @low: the low index of the sort range
+ * @high: the high index of the sort range
+ *
+ * Return: void
+ */
+size_t lomuto_partition(int *array, size_t size, ssize_t low, ssize_t high)
+{
+	int i, j, pivot = array[high];
+
+	for (i = j = low; j < high; j++)
+		if (array[j] < pivot)
+			swap(array, size, &array[j], &array[i++]);
+	swap(array, size, &array[i], &array[high]);
+
+	return (i);
+}
+
+/**
+ * quicksort - quicksorts via lomuto partitioning scheme
+ * @array: the integer array to sort
+ * @size: the size of the array
+ * @low: the low index of the sort range
+ * @high: the hig index of the sort range
+ *
+ * Return: void
+ */
+void quicksort(int *array, size_t size, ssize_t low, ssize_t high)
+{
+	if (low < high)
+	{
+		size_t p = lomuto_partition(array, size, low, high);
+
+		quicksort(array, size, low, p - 1);
+		quicksort(array, size, p + 1, high);
+	}
+}
+
+/**
+ * quick_sort - calls quicksort
+ * @array: the integer array to sort
+ * @size: the size of the array
+ *
+ * Return: void
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (!array || !size)
+		return;
+	quicksort(array, size, 0, size - 1);
+}
