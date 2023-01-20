e78fe2d36f18f91583056e68cbd0b1228a35505c
John Diginee <johndbizz@gmail.com>
2023-01-20 10:50:08 -0800

    Fixed some bugs in 3-quick_sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,77 +1,81 @@
 #include "sort.h"
 
-/**
- * swap - Swap two integers in an array
- * @array: the integer array to sort
- * @size: the size of the array
- * @a: The address of first value
- * @b: The address of second value
- *
- * Return: void
- */
-void swap(int *array, size_t size, int *a, int *b)
-{
-	if (*a != *b)
-	{
-		*a = *a + *b;
-		*b = *a - *b;
-		*a = *a - *b;
-		print_array((const int *)array, size);
-	}
-}
+void quick_sort_helper(int *array, int lo, int hi, size_t size);
+int partition(int *array, int lo, int hi, size_t size);
 
 /**
- * lomuto_partition - partitions the array
- * @array: the integer array to sort
- * @size: the size of the array
- * @low: the low index of the sort range
- * @high: the high index of the sort range
+ * quick_sort - function to sort using quick sort
  *
- * Return: void
- */
-size_t lomuto_partition(int *array, size_t size, ssize_t low, ssize_t high)
-{
-	int i, j, pivot = array[high];
-
-	for (i = j = low; j < high; j++)
-		if (array[j] < pivot)
-			swap(array, size, &array[j], &array[i++]);
-	swap(array, size, &array[i], &array[high]);
-
-	return (i);
-}
-
-/**
- * quicksort - quicksorts via lomuto partitioning scheme
- * @array: the integer array to sort
- * @size: the size of the array
- * @low: the low index of the sort range
- * @high: the hig index of the sort range
- *
- * Return: void
- */
-void quicksort(int *array, size_t size, ssize_t low, ssize_t high)
-{
-	if (low < high)
-	{
-		size_t p = lomuto_partition(array, size, low, high);
-
-		quicksort(array, size, low, p - 1);
-		quicksort(array, size, p + 1, high);
-	}
-}
-
-/**
- * quick_sort - calls quicksort
- * @array: the integer array to sort
- * @size: the size of the array
- *
- * Return: void
+ * @array: array to sort
+ * @size: the length of the array
  */
 void quick_sort(int *array, size_t size)
 {
-	if (!array || !size)
+	if (array == NULL || size < 2)
 		return;
-	quicksort(array, size, 0, size - 1);
+
+	quick_sort_helper(array, 0, size - 1, size);
 }
 
+/**
+ * quick_sort_helper - helper to quick sort
+ *
+ * @array: array to sort
+ * @lo: lowest value in array
+ * @hi: highest value in array
+ * @size: length of array
+ */
+void quick_sort_helper(int *array, int lo, int hi, size_t size)
+{
+	int p;
+
+	if (lo < hi)
+	{
+		p = partition(array, lo, hi, size);
+		quick_sort_helper(array, lo, p - 1, size);
+		quick_sort_helper(array, p + 1, hi, size);
+	}
+}
+
+/**
+ * partition - function to partition the array in smaller arrays
+ *
+ * @array: array to sort
+ * @lo: low value
+ * @hi: high value
+ * @size: length of array
+ *
+ * Return: the new value
+ */
+int partition(int *array, int lo, int hi, size_t size)
+{
+	int i, j;
+	int pivot;
+	int swap = 0;
+
+	pivot = array[hi];
+	i = lo - 1;
+
+	for (j = lo; j < hi; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (array[i] != array[j])
+			{
+				swap = array[i];
+				array[i] = array[j];
+				array[j] = swap;
+				print_array(array, size);
+			}
+		}
+	}
+	if (array[i + 1] != array[hi])
+	{
+		swap = array[i + 1];
+		array[i + 1] = array[hi];
+		array[hi] = swap;
+		print_array(array, size);
+	}
+	return (i + 1);
+}
